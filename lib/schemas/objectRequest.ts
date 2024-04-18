292de29c8c995daad5844c90f9111e4c282d2c23
Brad Brown <bradbrown@bradbrown.llc>
2024-04-18 10:32:21 -0400

    rename schemas to schema

--- lib/schemas/objectRequest.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "https://deno.land/x/zod@v3.22.4/mod.ts";
-import { base } from './base.ts'
-
-export const objectRequest = base.and(z.object({
-  method: z.string(),
-  params: z.object({}).passthrough(),
-}));
