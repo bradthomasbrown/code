e80e20b7dacc5e2afe89d209bf2968afe9b28ad4
Brad Brown <bradbrown@bradbrown.llc>
2024-04-23 15:28:28 -0400

    responsd: bigint to hex string

--- lib/respond.ts
+++ lib/respond.ts
@@ -3,9 +3,15 @@ type RespondArgs = {
   id:number|string|null
 }
 
+function replacer(_key:unknown, value:unknown) {
+  typeof value == 'bigint'
+    ? `0x${value.toString(16)}`
+    : value
+}
+
 export function respond({ result, id }:RespondArgs) {
   const jsonrpc = '2.0'
-  const body:BodyInit = JSON.stringify({ jsonrpc, result, id })
+  const body:BodyInit = JSON.stringify({ jsonrpc, result, id }, replacer)
   const headers = { 'Content-Type': 'application/json' }
   const status = 200
   const init:ResponseInit = { status, headers }
