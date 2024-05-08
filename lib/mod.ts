102967a70cb553f650a4ada4cd61b935fda1cb6c
Brad Brown <bradbrown@bradbrown.llc>
2024-05-08 00:30:05 -0400

    lib barrel

--- lib/mod.ts
+++ lib/mod.ts
@@ -1,4 +1,6 @@
-export * from './respond.ts'
-export * from './error.ts'
-export * as schema from './schema/mod.ts'
-export * as types from './types/mod.ts'
+import { json } from "lib/json.ts"
+export { json }
+export * from "lib/Client.ts"
+export * from "lib/Server.ts"
+export * as schemas from "schemas/mod.ts"
+export * as types from "types/mod.ts"
