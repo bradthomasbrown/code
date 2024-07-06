e8b2f9d5e951ef00297bef55ba0a2d0ad89fa09a
Brad Brown <bradbrown@bradbrown.llc>
2024-07-06 18:49:09 +0000

    require exitHandlers, formatting

--- Docker.ts
+++ Docker.ts
@@ -1,24 +1,24 @@
-import { exitHandlers } from './exitHandlers.ts'
+import { ExitHandlers } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/ExitHandlers@0.0.0/mod.ts'
 import * as schemas from './schemas/mod.ts'
 import * as jra from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/jra@0.1.1/lib/mod.ts'
 
-type RunOptions = {
-    exitHandlers:(()=>void)[]
-}
-
 export class Docker {
 
-    static async run(image:string, options?:RunOptions) {
+    static async run(image:string, exitHandlers:ExitHandlers) {
         const args = ['run', '-d', '--rm', image]
-        const cmdOut = await new Deno.Command('docker', { args, stdout: 'piped', stderr: 'piped' }).output()
+        const stderr = 'piped', stdout = 'piped'
+        const options = { args, stderr, stdout } as const
+        const cmdOut = await new Deno.Command('docker', options).output()
         const id = new TextDecoder().decode(cmdOut.stdout).replace('\n', '')
-        ;(options?.exitHandlers ?? exitHandlers).push(() => Docker.stop(id))
+        exitHandlers.push(() => Docker.stop(id))
         return id
     }
 
     static async inspect(id:string) {
         const args = ['inspect', id]
-        const cmdOut = await new Deno.Command('docker', { args, stdout: 'piped', stderr: 'piped' }).output()
+        const stderr = 'piped', stdout = 'piped'
+        const options = { args, stderr, stdout } as const
+        const cmdOut = await new Deno.Command('docker', options).output()
         const out = new TextDecoder().decode(cmdOut.stdout)
         const jsonParseResult = jra.schemas.json.parse(JSON.parse(out))
         const dockerInspectParseResult = schemas.Inspection.parse(jsonParseResult)
@@ -26,7 +26,10 @@ export class Docker {
     }
 
     static stop(id:string) {
-        new Deno.Command('docker', { args: ['stop', id], stdout: 'piped', stderr: 'piped' }).output()
+        const args = ['stop', id]
+        const stderr = 'piped', stdout = 'piped'
+        const options = { args, stderr, stdout } as const
+        new Deno.Command('docker', options).output()
     }
 
 }
