514d4027f47071cd9780bebb8b398904032b3e74
Brad Brown <bradbrown@bradbrown.llc>
2024-07-26 16:33:40 +0000

    use newer exit handlers

--- Docker.ts
+++ Docker.ts
@@ -1,4 +1,4 @@
-import { ExitHandlers } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/ExitHandlers@0.0.0/mod.ts'
+import { ExitHandlers } from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/ExitHandlers@0.0.1/mod.ts'
 import * as schemas from './schemas/mod.ts'
 import * as jra from 'https://cdn.jsdelivr.net/gh/bradbrown-llc/jra@0.1.1/lib/mod.ts'
 
@@ -18,8 +18,12 @@ export class Docker {
         const args = ['run', '--rm', image]
         const stderr = 'piped', stdout = 'piped'
         const options = { args, stderr, stdout } as const
-        const cmdOut = await new Deno.Command('docker', options).output()
-        return new TextDecoder().decode(cmdOut.stdout)
+        const cmd = new Deno.Command('docker', options)
+        const proc = cmd.spawn()
+        let buf = new Uint8Array([])
+        for await (const values of proc.stdout.values()) buf = new Uint8Array([...buf, ...values])
+        await proc.status
+        return new TextDecoder().decode(buf)
     }
 
     static async inspect(id:string) {
