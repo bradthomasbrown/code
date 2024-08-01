212db5c6f065625f58ac21d54673366d4dfc1dca
Brad Brown <bradbrown@bradbrown.llc>
2024-08-01 20:58:40 +0000

    successful abi signatureComponentMap generating

---
+++ abi/tests/generate/Resolver/test.ts
@@ -0,0 +1,8 @@
+import { fromFileUrl } from 'https://deno.land/std@0.224.0/path/mod.ts'
+import { compile } from "../../../../solc/lib/compile.ts";
+
+const solcJsonInputPath = fromFileUrl(import.meta.resolve('../../../../common/@dizzyhavoc/contracts/Resolver/settings.json'))
+const solcJsonOutput = await compile(solcJsonInputPath)
+const abi = solcJsonOutput.abi('Resolver')
+
+console.log(abi.signatureComponentsMap())
