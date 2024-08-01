eca278065f2ea9157df275ed0f88c7a8df5edb1b
Brad Brown <bradbrown@bradbrown.llc>
2024-08-01 16:41:14 +0000

    cache the solc list, if older than a day or doesn't exist, reacquire

--- solc/lib/_fetchSolcList.ts
+++ solc/lib/_fetchSolcList.ts
@@ -2,7 +2,14 @@ import { SolcListObject } from "../types/_SolcListObject.ts";
 import { solcListObject } from "../schemas/_solcListObject.ts"
 
 export async function fetchSolcList():Promise<SolcListObject> {
-    const response = await fetch('https://binaries.soliditylang.org/linux-amd64/list.json')
-    const text = await response.text()
-    return solcListObject.parse(JSON.parse(text))
+    const cachePath = `${Deno.env.get('HOME')}/.kaaos/solc/list.json`
+    const fileInfo = await Deno.stat(cachePath).catch(() => undefined)
+    const unmodifiedAge = fileInfo?.mtime ? Date.now() - fileInfo.mtime.getTime() : Infinity
+    if (!fileInfo || unmodifiedAge >= 1000 * 3600 * 60 * 24) {
+        const response = await fetch('https://binaries.soliditylang.org/linux-amd64/list.json')
+        const blob = await response.blob()
+        await Deno.writeFile(cachePath, blob.stream(), { mode: 0o755 })
+    }
+    const solcListText = await Deno.readTextFile(cachePath)
+    return solcListObject.parse(JSON.parse(solcListText))
 }
