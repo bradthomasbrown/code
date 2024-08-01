444b2f857aef7a778dde771cfef4e2fd1dbe667a
Brad Brown <bradbrown@bradbrown.llc>
2024-08-01 20:58:23 +0000

    recursion with components (for later)

--- abi/schemas/_abiInputObject.ts
+++ abi/schemas/_abiInputObject.ts
@@ -1,6 +1,15 @@
 import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
 
-export const abiInputObject = z.object({
+const baseAbiInputObject = z.object({
     name: z.string(),
-    type: z.string()
+    type: z.string(),
+    internalType: z.string()
+})
+
+type AbiInputObject = z.infer<typeof baseAbiInputObject> & {
+    components?: AbiInputObject[]
+}
+
+export const abiInputObject:z.ZodType<AbiInputObject> = baseAbiInputObject.extend({
+    components: z.lazy(() => abiInputObject.array().optional())
 })
