fb539abeb65d5750b853d2058c6d2d55bb854a68
Brad Brown <bradbrown@bradbrown.llc>
2024-08-02 02:23:09 +0000

    name can be missing (public top level mapping), type can be missing, internalType can be missing (old ABI)

--- abi/schemas/_abiInputObject.ts
+++ abi/schemas/_abiInputObject.ts
@@ -1,9 +1,9 @@
 import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
 
 const baseAbiInputObject = z.object({
-    name: z.string(),
+    name: z.string().optional(),
     type: z.string(),
-    internalType: z.string()
+    internalType: z.string().optional()
 })
 
 type AbiInputObject = z.infer<typeof baseAbiInputObject> & {
