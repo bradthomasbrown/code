58554e14a79cc65d0aaed71ac17475da9add1b63
Brad Brown <bradbrown@bradbrown.llc>
2024-08-02 02:21:04 +0000

    shuffling and new specific tests

--- abi/tests/generate/UniswapV2Factory/test.ts
+++ abi/tests/generate/UniswapV2Factory/test.ts
@@ -1,8 +1,10 @@
 import { fromFileUrl } from 'https://deno.land/std@0.224.0/path/mod.ts'
 import { compile } from "../../../../solc/lib/compile.ts";
 
-const solcJsonInputPath = fromFileUrl(import.meta.resolve('../../../../common/@uniswap/v2-core/contracts/settings.json'))
-const solcJsonOutput = await compile(solcJsonInputPath)
+const targetDir = fromFileUrl(import.meta.resolve('../../../../common/@uniswap/v2-core/contracts'))
+const solcJsonInputPath = fromFileUrl(import.meta.resolve('./settings.json'))
+const solcJsonOutput = await compile(solcJsonInputPath, targetDir)
 const abi = solcJsonOutput.abi('UniswapV2Factory')
 
+console.log(solcJsonOutput.toString())
 console.log(abi.signatureComponentsMap())
--- abi/tests/generate/UniswapV3Factory/test.ts
+++ abi/tests/generate/UniswapV3Factory/test.ts
@@ -1,10 +1,10 @@
 import { fromFileUrl } from 'https://deno.land/std@0.224.0/path/mod.ts'
 import { compile } from "../../../../solc/lib/compile.ts";
 
-const targetDir = fromFileUrl(import.meta.resolve('../../../../common/WETH9'))
+const targetDir = fromFileUrl(import.meta.resolve('../../../../common/@uniswap/v3-core/contracts'))
 const solcJsonInputPath = fromFileUrl(import.meta.resolve('./settings.json'))
 const solcJsonOutput = await compile(solcJsonInputPath, targetDir)
-const abi = solcJsonOutput.abi('WETH9')
+const abi = solcJsonOutput.abi('UniswapV3Factory')
 
 console.log(solcJsonOutput.toString())
 console.log(abi.signatureComponentsMap())
