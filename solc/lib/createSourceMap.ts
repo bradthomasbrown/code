e92cd1b4b976f5b549245d8e84a0a8f77e268e66
Brad Brown <bradbrown@bradbrown.llc>
2024-08-05 16:53:46 +0000

    fancy new function to make a source map for solc json input

---
+++ solc/lib/createSourceMap.ts
@@ -0,0 +1,73 @@
+import { WalkOptions, walk } from "https://deno.land/std@0.224.0/fs/walk.ts";
+import { dirname, resolve } from "https://deno.land/std@0.224.0/path/mod.ts";
+import { globToRegExp } from "https://deno.land/std@0.224.0/path/glob_to_regexp.ts";
+
+export async function createSourceMap({
+    requiredSources,
+    basePath,
+    includePaths,
+    excludePaths,
+    remappings
+}:{
+    requiredSources: string|[string, ...string[]]
+    basePath?: string
+    includePaths?: string|string[]
+    excludePaths?: string[]
+    remappings?: string|string[]|string[][]
+}) {
+
+    const normalizedRequiredSources = typeof requiredSources == 'string'
+        ? requiredSources.split(',')
+        : requiredSources
+    const normalizedIncludePaths = typeof includePaths == 'string'
+        ? includePaths.split(',')
+        : includePaths
+        ?? []
+    const normalizedRemappings = typeof remappings == 'string'
+        ? remappings.split(',').map(remapping => remapping.split('='))
+        : typeof remappings?.[0] == 'string' ? (remappings as string[]).map(remapping => remapping.split('='))
+        : (remappings as string[][])
+        ?? []
+    const roots = [basePath ?? Deno.cwd(), ...(normalizedIncludePaths ?? [])]
+    const requiredSourcesSet = new Set<string>([...normalizedRequiredSources])
+    const sourceToCodeMap = new Map<string,string>()
+    const sourceToPathMap = new Map<string,string>()
+
+    const applyRemappings = (source:string) => normalizedRemappings.reduce((source, [from, to]) => source.replace(from, to), source)
+    const pathToSource = (path: string, root:string) => path.replace(`${root == basePath ? root : dirname(root)}/`, '')
+    const codeToImportLines = (code: string):string[] => code.match(/^import\b.+?(['"])(.+)\1/mg) ?? []
+    const importLineToImportPath = (importLine: string) => applyRemappings(importLine.match(/^import\b.+?(['"])(.+)\1/)![2])
+    const codeToImportPaths = (code:string) => codeToImportLines(code).map(importLineToImportPath)
+    const sourceResolve = (rootSource:string, pathSegment:string) => resolve(dirname(`/${rootSource}`), pathSegment).replace(/^\//, '')
+    const isAbsoluteSource = (source:string) => !source.match(/^\./)
+    const sourceResolver = (rootSource:string) => (pathSegment:string) => isAbsoluteSource(pathSegment) ? pathSegment : sourceResolve(rootSource, pathSegment)
+    const getImportSources = (code:string, rootSource:string) => codeToImportPaths(code).map(sourceResolver(rootSource))
+
+    const fulfillSourceRequirement = async (source:string) => {
+        const path = sourceToPathMap.get(source)!
+        const code = await Deno.readTextFile(path)
+        sourceToCodeMap.set(source, code)
+        for (const importSource of getImportSources(code, source))
+            if (!sourceToPathMap.has(importSource)) requiredSourcesSet.add(importSource)
+            else if (!sourceToCodeMap.has(importSource)) await fulfillSourceRequirement(importSource)
+        requiredSourcesSet.delete(source)
+    }
+
+    const handlePath = async (path:string, root:string) => {
+        const source = pathToSource(path, root)
+        sourceToPathMap.set(source, path)
+        if (requiredSourcesSet.has(source)) await fulfillSourceRequirement(source)
+        return requiredSourcesSet.size
+    }
+
+    const walkRoot = async (root:string) => {
+        const skip = (excludePaths ?? []).map(glob => globToRegExp(glob))
+        const walkOptions: WalkOptions = { includeDirs: false, exts: ['.sol'], skip }
+        for await (const { path } of walk(root, walkOptions)) if (!await handlePath(path, root)) return true
+    }
+
+    for (const root of roots) if (await walkRoot(root)) break
+    if (!requiredSourcesSet.size) return sourceToCodeMap
+    throw new Error('missing imports', { cause: { requiredSourcesSet } })
+    
+} 
