decc270e120201da5996d7820d53977cdcc0ede6
Brad Brown <bradbrown@bradbrown.llc>
2024-08-05 21:28:27 +0000

    precise

--- solc/schemas/_solcListObject.ts
+++ solc/schemas/_solcListObject.ts
@@ -1,7 +1,17 @@
 import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
 
 export const solcListObject = z.object({
-    releases: z.record(z.string())
+    builds: z.object({
+        path: z.string(),
+        version: z.string(),
+        build: z.string(),
+        longVersion: z.string(),
+        keccak256: z.string(),
+        sha256: z.string(),
+        urls: z.string().array()
+    }).array(),
+    releases: z.record(z.string()),
+    latestRelease: z.string() 
 }).strict()
 
 export const solcListObjectFromString = z.string().transform(s => solcListObject.parse(JSON.parse(s)))
