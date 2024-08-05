0b9d9a40a963ca283fc6bc78e87413d248475fb9
Brad Brown <bradbrown@bradbrown.llc>
2024-08-05 21:28:18 +0000

    no longer needed

--- solc/schemas/_solcJsonInputObject.ts
+++
@@ -1,7 +0,0 @@
-import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
-
-export const solcJsonInputObject = z.object({
-    sources: z.record(z.object({
-        urls: z.string().array()
-    }))
-}).passthrough()
