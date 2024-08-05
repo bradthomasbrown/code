09329bdeffe1efdd846afa04ad0da58d39374f4f
Brad Brown <bradbrown@bradbrown.llc>
2024-08-05 21:26:25 +0000

    replace with something more similar to _SolcList

--- solc/lib/_fetchSolcRelease.ts
+++
@@ -1,9 +0,0 @@
-export async function fetchSolcRelease(release:string):Promise<void> {
-    const cachePath = `${Deno.env.get('HOME')}/.kaaos/solc/${release}`
-    const fileInfo = await Deno.stat(cachePath).catch(() => undefined)
-    if (!fileInfo) {
-        const response = await fetch(`https://binaries.soliditylang.org/linux-amd64/${release}`)
-        const blob = await response.blob()
-        await Deno.writeFile(cachePath, blob.stream(), { mode: 0o755 })
-    }
-}
