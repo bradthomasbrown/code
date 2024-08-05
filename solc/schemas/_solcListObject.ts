d7ca7487adbf5c5b8c4f4a11fec0351870d3f1c1
Brad Brown <bradbrown@bradbrown.llc>
2024-08-05 16:54:27 +0000

    make strict and add option to parse from a json string

--- solc/schemas/_solcListObject.ts
+++ solc/schemas/_solcListObject.ts
@@ -2,4 +2,6 @@ import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
 
 export const solcListObject = z.object({
     releases: z.record(z.string())
-})
+}).strict()
+
+export const solcListObjectFromString = z.string().transform(s => solcListObject.parse(JSON.parse(s)))
