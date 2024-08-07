73e3267dad71cc44e1baae0b9ecb5eaa3cc25c86
Brad Brown <bradbrown@bradbrown.llc>
2024-08-07 16:22:54 +0000

    schema style enforcement: all objects should be strict, anything ending with an s should be an array or a record or similar. isolate and simplify

--- solc/schemas/_solcListObject.ts
+++ solc/schemas/_solcListObject.ts
@@ -1,17 +1,23 @@
 import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
 
-export const solcListObject = z.object({
-    builds: z.object({
-        path: z.string(),
-        version: z.string(),
-        build: z.string(),
-        longVersion: z.string(),
-        keccak256: z.string(),
-        sha256: z.string(),
-        urls: z.string().array()
-    }).array(),
-    releases: z.record(z.string()),
+const build = z.object({
+    path: z.string(),
+    version: z.string(),
+    build: z.string(),
+    longVersion: z.string(),
+    keccak256: z.string(),
+    sha256: z.string(),
+    urls: z.string().array()
+}).strict()
+
+const builds = build.optional().array()
+
+export const solcListFromObject = z.object({
+    builds: builds,
+    releases: z.record(z.string().optional()),
     latestRelease: z.string() 
 }).strict()
 
-export const solcListObjectFromString = z.string().transform(s => solcListObject.parse(JSON.parse(s)))
+const solcListFromString = z.string().transform(s => solcListFromObject.parse(JSON.parse(s)))
+
+export const solcList = solcListFromString.or(solcListFromObject)
