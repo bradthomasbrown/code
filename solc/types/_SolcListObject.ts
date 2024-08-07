653a3f8ae2d6b886cb96cf8945611e2e97ea1e9b
Brad Brown <bradbrown@bradbrown.llc>
2024-08-07 16:23:34 +0000

    this type does need to be differentiated from the class. specifically it is for an object

--- solc/types/_SolcListObject.ts
+++ solc/types/_SolcListObject.ts
@@ -1,4 +1,4 @@
 import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
-import { solcListObject } from '../schemas/_solcListObject.ts';
+import { solcListFromObject } from '../schemas/_solcListObject.ts';
 
-export type SolcListObject = z.infer<typeof solcListObject>
+export type SolcListObject = z.infer<typeof solcListFromObject>
