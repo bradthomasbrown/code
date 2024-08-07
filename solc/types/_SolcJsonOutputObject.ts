aaa79b3dbb8dd1cb8b82f13e7414c719ea1c7729
Brad Brown <bradbrown@bradbrown.llc>
2024-08-07 16:27:00 +0000

    remove

--- solc/types/_SolcJsonOutputObject.ts
+++
@@ -1,4 +0,0 @@
-import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
-import { solcJsonOutputObject } from '../schemas/_solcJsonOutputObject.ts';
-
-export type SolcJsonOutputObject = z.infer<typeof solcJsonOutputObject>
