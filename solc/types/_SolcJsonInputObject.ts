135a8a7a77866bd09c153718d4e7d12509eb2c7f
Brad Brown <bradbrown@bradbrown.llc>
2024-08-07 16:24:01 +0000

    deprecate

--- solc/types/_SolcJsonInputObject.ts
+++
@@ -1,4 +0,0 @@
-import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
-import { solcJsonInputObject } from '../schemas/_solcJsonInputObject.ts';
-
-export type SolcJsonInputObject = z.infer<typeof solcJsonInputObject>
