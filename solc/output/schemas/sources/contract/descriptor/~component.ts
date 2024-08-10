e77faedc714e47d73c8b3ad73da427534cc8d81f
Brad Brown <bradbrown@bradbrown.llc>
2024-08-10 16:54:07 +0000

    Component is the only type, and we can pull it down without breaking a concept

--- solc/output/schemas/sources/contract/descriptor/~component.ts
+++ solc/output/schemas/sources/contract/descriptor/~component.ts
@@ -1,5 +1,5 @@
 import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
-import { Component } from "solc/output/schemas/sources/contract/descriptor/types/Component.ts"
+import { Component } from "solc/output/types/Component.ts"
 
 export const component: z.ZodType<Component> = z.object({
     name: z.string(),
