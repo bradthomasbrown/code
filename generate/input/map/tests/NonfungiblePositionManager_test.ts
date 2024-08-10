7f3727d7067fb8e770471310096a842a9e1c6a03
Brad Brown <bradbrown@bradbrown.llc>
2024-08-10 17:09:25 +0000

    working generate/input/map map and verify tests

--- generate/input/map/tests/NonfungiblePositionManager_test.ts
+++ generate/input/map/tests/NonfungiblePositionManager_test.ts
@@ -1,11 +1,11 @@
 import { assert } from 'https://deno.land/std@0.224.0/assert/assert.ts';
 import { load } from "solc/compile/fixtures/lib/load.ts"
-import { compile } from "solc/compile/tests/lib/ERC20_compile.ts"
+import { compile } from "solc/compile/tests/lib/NonfungiblePositionManager_compile.ts"
 import { output as outputSchema } from 'solc/output/schemas/mod.ts';
 import { map } from 'generate/input/map/lib/mod.ts';
 import { validate } from "generate/input/map/lib/~validate.ts";
 
-const outputString = await load('ERC20', compile)
+const outputString = await load('NonfungiblePositionManager', compile)
 const output = outputSchema.parse(outputString)
 const outputSources = output.contracts
 assert(outputSources)
