f30485db623d0b05692ec3990c39b306af8d9da9
Brad Brown <bradbrown@bradbrown.llc>
2024-08-10 17:09:06 +0000

    fixtures are from compilation, makes more sense to put them under solc/compile, not solc/output

--- solc/compile/fixtures/lib/load.ts
+++ solc/compile/fixtures/lib/load.ts
@@ -1,4 +1,4 @@
-import { cache } from 'solc/output/fixtures/lib/cache.ts'
+import { cache } from "solc/compile/fixtures/lib/cache.ts"
 
 export async function load(name:string, compile:()=>Promise<string>) {
     const path = `${name}.json`
