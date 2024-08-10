1f2c626188ccb683b476cfc08c739febfa4368c3
Brad Brown <bradbrown@bradbrown.llc>
2024-08-10 16:27:31 +0000

    when we do that, these tests are the only files in their dirs. so we pull them down. names collide again, we prefix again.

--- solc/compile/tests/ERC20/test.ts
+++
@@ -1,3 +0,0 @@
-import { compile } from "solc/compile/tests/ERC20/compile.ts"
-
-Deno.test('compile', async () => { await compile() })
--- solc/compile/tests/NFTDescriptor/test.ts
+++
@@ -1,3 +0,0 @@
-import { compile } from "solc/compile/tests/NFTDescriptor/compile.ts"
-
-Deno.test('compile', async () => { await compile() })
