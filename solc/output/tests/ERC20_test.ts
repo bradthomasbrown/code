dcad2874a6362fbb01c0a60bf51d5deaddac5fac
Brad Brown <bradbrown@bradbrown.llc>
2024-08-10 16:28:37 +0000

    now we need to update these imports

--- solc/output/tests/ERC20_test.ts
+++ solc/output/tests/ERC20_test.ts
@@ -1,6 +1,6 @@
 import { output as outputSchema } from 'solc/output/schemas/mod.ts';
 import { load } from 'solc/output/fixtures/lib/load.ts'
-import { compile } from "solc/compile/tests/ERC20/compile.ts"
+import { compile } from "solc/compile/tests/lib/ERC20_compile.ts"
 
 const outputString = await load('ERC20', compile)
 
