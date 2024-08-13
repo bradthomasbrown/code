ce791592bd15ae4a25ec5ab573c94652edd51706
Brad Brown <bradbrown@bradbrown.llc>
2024-08-13 18:23:09 +0000

    fold to ExtendedSet, consistent with ExtendedMap

--- map/lib/SetMap.ts
+++
@@ -1,9 +0,0 @@
-import { ExtendedMap } from "map/lib/ExtendedMap.ts";
-
-export class SetMap<Ka, Va extends Set<unknown>> extends ExtendedMap<Ka, Va> {
-
-    constructor(...parameters:ConstructorParameters<typeof Map<Ka, Va>>) {
-        super(...parameters)
-    }
-
-}
