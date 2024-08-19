d85441cc7bad80c769efeae96cdbf08dc77b8e35
Brad Brown <bradbrown@bradbrown.llc>
2024-08-19 16:16:13 +0000

    instead of compile in tests/lib, export params in test. remove redundant name suffix

---
+++ deno/evm/solc/compile/tests/ERC20.ts
@@ -0,0 +1,10 @@
+import { compile } from '../lib/mod.ts'
+import { Params } from '../types/Parameters.ts'
+import { fromFileUrl } from 'https://deno.land/std@0.224.0/path/from_file_url.ts'
+
+export const params: Params = {
+    targets: { 'ERC20.sol': ['ERC20'] },
+    basePath: fromFileUrl(import.meta.resolve('../../../../../smartks/dizzyhavoc/ERC20'))
+}
+
+Deno.test('compile ERC20', async () => { await compile(params) })
--- deno/evm/solc/compile/tests/ERC20_test.ts
+++
@@ -1,3 +0,0 @@
-import { compile } from "solc/compile/tests/lib/ERC20_compile.ts"
-
-Deno.test('compile', async () => { await compile() })
