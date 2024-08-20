64f1d654f4aff1b188e9d8fa7f7d8dba6acbb8de
Brad Brown <bradbrown@bradbrown.llc>
2024-08-20 04:07:33 +0000

    use stdplus Cache instead

--- deno/evm/solc/compile/fixtures/lib/_cache.ts
+++
@@ -1,5 +0,0 @@
-import { resolve } from 'https://deno.land/std@0.224.0/path/resolve.ts';
-import { Cache } from 'std/cache/Cache.ts';
-
-const root = resolve(import.meta.dirname!, '..')
-export const cache = new Cache(root)
