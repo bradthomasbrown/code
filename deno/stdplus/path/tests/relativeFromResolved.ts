dc6e623ef1b2b614c5b5ee7fe062231b0139a303
Brad Brown <bradbrown@bradbrown.llc>
2024-08-20 03:15:51 +0000

    funky tests

---
+++ deno/stdplus/path/tests/relativeFromResolved.ts
@@ -0,0 +1,33 @@
+import { assertEquals } from 'https://deno.land/std@0.224.0/assert/assert_equals.ts';
+import { wrapImportMeta } from '../lib/relativeFromResolved.ts'
+import { fromFileUrl } from 'https://deno.land/std@0.224.0/path/from_file_url.ts';
+
+Deno.test('intuitive', () => {
+    const wrappedImportMeta = wrapImportMeta(import.meta)
+    const baseResolverA = wrappedImportMeta.baseResolver('./A/')
+    const baseResolverB = baseResolverA.cd('../B/')
+    const pathA = baseResolverA.resolve('a.ts')
+    const pathB = baseResolverB.resolve('b.ts')
+    assertEquals(
+        [pathA, pathB],
+        [
+            fromFileUrl(import.meta.resolve('./A/a.ts')),
+            fromFileUrl(import.meta.resolve('./B/b.ts'))
+        ]
+    )
+})
+
+Deno.test('concise', () => {
+    const i = wrapImportMeta(import.meta)
+    const A = i`./A/`
+    const B = A.cd`../B/`
+    const pathA = A`a.ts`
+    const pathB = B`b.ts`
+    assertEquals(
+        [pathA, pathB],
+        [
+            fromFileUrl(import.meta.resolve('./A/a.ts')),
+            fromFileUrl(import.meta.resolve('./B/b.ts'))
+        ]
+    )
+})
