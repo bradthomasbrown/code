f64193e5ca6d614b929af051e3aba1c1b92ec458
Brad Brown <bradbrown@bradbrown.llc>
2024-08-27 10:52:28 +0000

    fold into sets of name sharing descriptors

--- deno/evm/solc/output/schemas/sources/contract/abi.ts
+++ deno/evm/solc/output/schemas/sources/contract/abi.ts
@@ -1,3 +1,15 @@
 import { descriptor } from "./descriptor/mod.ts"
+import { Descriptor } from '../../../types/mod.ts'
 
-export const abi = descriptor.array()
+export const abi = descriptor.array().transform(descriptorArray => {
+    const abi = new Map<string, Descriptor[]>()
+    for (const descriptor of descriptorArray) {
+        const name = 'name' in descriptor ? descriptor.name : descriptor.type
+        if (abi.has(name)) {
+            abi.get(name)!.push(descriptor)
+        } else {
+            abi.set(name, [descriptor])
+        }
+    }
+    return abi
+})
