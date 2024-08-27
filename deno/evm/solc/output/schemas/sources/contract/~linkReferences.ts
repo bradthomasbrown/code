42a4973e2eb303498271df0546aacb29c7f853e4
Brad Brown <bradbrown@bradbrown.llc>
2024-08-27 10:51:55 +0000

    use extended maps

--- deno/evm/solc/output/schemas/sources/contract/~linkReferences.ts
+++ deno/evm/solc/output/schemas/sources/contract/~linkReferences.ts
@@ -1,4 +1,5 @@
 import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
+import { ExtendedMap } from "../../../../../../stdplus/map/lib/ExtendedMap.ts"
 import { sources } from "./~sources.ts"
 
-export const linkReferences = z.record(z.string(), sources).transform(r => new Map(Object.entries(r)))
+export const linkReferences = z.record(z.string(), sources).transform(r => new ExtendedMap(Object.entries(r)))
--- deno/evm/solc/output/schemas/sources/contract/~sources.ts
+++ deno/evm/solc/output/schemas/sources/contract/~sources.ts
@@ -1,4 +1,5 @@
 import { z } from 'https://deno.land/x/zod@v3.23.8/mod.ts'
+import { ExtendedMap } from "../../../../../../stdplus/map/lib/ExtendedMap.ts"
 import { byteOffsets } from "./~byteOffsets.ts"
 
-export const sources = z.record(z.string(), byteOffsets).transform(r => new Map(Object.entries(r)))
+export const sources = z.record(z.string(), byteOffsets).transform(r => new ExtendedMap(Object.entries(r)))
