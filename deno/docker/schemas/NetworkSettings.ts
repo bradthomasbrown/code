22616a635c6800a7d2db62f9b3ac94f355ea4101
Brad Brown <bradbrown@bradbrown.llc>
2024-10-12 23:01:41 +0000

    probably shuffling

--- deno/docker/schemas/NetworkSettings.ts
+++
@@ -1,5 +0,0 @@
-import z from "https://deno.land/x/zod@v3.22.4/index.ts";
-
-export const NetworkSettings = z.object({
-    IPAddress: z.string()
-}) 
--- deno/docker/tests/hello-world.ts
+++
@@ -1,4 +0,0 @@
-import { Docker } from "../Docker.ts";
-
-const id = await Docker.run('hello-world')
-console.log(id)
